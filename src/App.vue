afd473e1c43dcf23825a1af3c0e37b12054907f5
wangxin <975548620@qq.com>
2019-05-08 10:20:46 +0800

    update all file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <olmap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import olmap from './components/olmap.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            olmap
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{padding:0; margin:0;}
+  html,body{
+    height: 100%;
+  }
+  #app {
+    height: 100%;
+  }
 </style>
